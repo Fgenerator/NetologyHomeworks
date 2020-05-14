bdca2ad255c3ee028c2c850da926444ec2492f12
Evgeny Ermakov <desteev@gmail.com>
2020-05-14 11:54:33 +0500

    Edited: calculation(sign, a, b), input_handling(input)

--- NetologyExceptions/main.py
+++ NetologyExceptions/main.py
@@ -1,50 +1,33 @@
 def calculation(sign, a, b):
-    if sign == '+':
-        return addition(a, b)
-    elif sign == '-':
-        return subtraction(a, b)
-    elif sign == '*':
-        return multiplication(a, b)
-    elif sign == '/':
-        return division(a, b)
-
-
-def addition(a, b):
-    return a + b
-
-
-def subtraction(a, b):
-    return a - b
-
-
-def multiplication(a, b):
-    return a * b
-
-
-def division(a, b):
+    ops = ['+', '-', '*', '/']
     try:
-        return a / b
-    except ZeroDivisionError:
-        print('Деление на ноль.')
+        assert sign in ops
+    except AssertionError:
+        print('Недопустимая операция.')
         main()
+    else:
+        if sign == '+':
+            return a + b
+        elif sign == '-':
+            return a - b
+        elif sign == '*':
+            return a * b
+        elif sign == '/':
+            try:
+                return a / b
+            except ZeroDivisionError:
+                print('Деление на ноль.')
+                main()
 
 
 def input_handling(input):
     input = input.split()
+    #print(calculation(input[0], int(input[1]), int(input[2])))
     try:
-        assert len(input) == 3
-    except AssertionError:
-        print('Передано неверное количество аргументов.')
+        print(calculation(*input))
+    except TypeError:
+        print('Передано неверное количество аргументов')
         main()
-    else:
-        ops = ['+', '-', '*', '/']
-        try:
-            assert input[0] in ops
-        except AssertionError:
-            print('Недопустимая операция.')
-            main()
-        else:
-            print(calculation(input[0], int(input[1]), int(input[2])))
 
 
 def main():
