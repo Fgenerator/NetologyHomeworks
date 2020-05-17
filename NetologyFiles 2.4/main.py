fad8ec4f4a079ee74cb0a777109d334d673d29a7
Evgeny Ermakov <desteev@gmail.com>
2020-05-17 19:04:05 +0500

    Minor edits

--- NetologyFiles 2.4/main.py
+++ NetologyFiles 2.4/main.py
@@ -22,12 +22,13 @@ def get_shop_list_by_dishes(dishes, person_count):
     shop_list = {}
     for dish in dishes:
         for ingredient in cook_book[dish]:
-            if ingredient['ingredient_name'] in shop_list:
-                shop_list[ingredient['ingredient_name']]['quantity'] += ingredient['quantity'] * person_count
+            name = ingredient['ingredient_name']
+            if name in shop_list:
+                shop_list[name]['quantity'] += ingredient['quantity'] * person_count
             else:
-                shop_list[ingredient['ingredient_name']] = {'measure': ingredient['measure'],
-                                                           'quantity': ingredient['quantity'] * person_count
-                                                           }
+                shop_list[name] = {'measure': ingredient['measure'],
+                                   'quantity': ingredient['quantity'] * person_count
+                                   }
     return shop_list
 
 
