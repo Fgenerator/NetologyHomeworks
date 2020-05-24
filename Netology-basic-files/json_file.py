b6effa05e5ad5454f40f988c41b78cf662e79882
Evgeny Ermakov <desteev@gmail.com>
2020-05-24 21:20:03 +0500

    Added functions open_json(file) get_words(data) count_words(words) sort_words_by_count(words_with_counts) main()

--- Netology-basic-files/json_file.py
+++ Netology-basic-files/json_file.py
@@ -1,6 +1,44 @@
 import json
 from pprint import pprint
 
-with open('newsafr.json', encoding='utf-8') as f:
-    data = json.load(f)
-    pprint(data)
+
+def open_json(file):
+    with open(file, encoding='utf-8') as f:
+        return json.load(f)
+
+
+def get_words(data):
+    words = []
+    for item in data['rss']['channel']['items']:
+        text = item['description'].split(' ')
+        for word in text:
+            if len(word) > 6:
+                words.append(word)
+    return words
+
+
+def count_words(words):
+    top_words = {}
+    for word in words:
+        if word in top_words:
+            top_words[word] += 1
+        else:
+            top_words[word] = 1
+    return top_words
+
+
+def sort_words_by_count(words_with_counts):
+    words_list = sorted(words_with_counts.items(), reverse=True, key=lambda item: item[1])
+    names = [name for name, count in words_list]
+    return names
+
+
+def main():
+    data = open_json('newsafr.json')
+    words = get_words(data)
+    words_with_counts = count_words(words)
+    sorted_words = sort_words_by_count(words_with_counts)
+    pprint(sorted_words[0:10])
+
+
+main()
