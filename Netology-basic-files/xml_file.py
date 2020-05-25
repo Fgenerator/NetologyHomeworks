d48c349f2f9937963a24531f49f3f41c97efa707
Evgeny Ermakov <desteev@gmail.com>
2020-05-25 14:24:27 +0500

    Added functions:
    get_tree_from_xml(file)
    get_items_from_tree(tree, path)
    get_texts_from_items(items, field)
    get_words_from_texts(texts)

--- Netology-basic-files/xml_file.py
+++ Netology-basic-files/xml_file.py
@@ -1,8 +1,46 @@
 import xml.etree.ElementTree as ET
+from collections import Counter
+from pprint import pprint
 
-parser = ET.XMLParser(encoding='utf-8')
-tree = ET.parse('newsafr.xml', parser)
 
-root = tree.getroot()
+def get_tree_from_xml(file):
+    parser = ET.XMLParser(encoding='utf-8')
+    tree = ET.parse(file, parser)
+    return tree
 
-print(root.attrib)
+
+def get_items_from_tree(tree, path):
+    root = tree.getroot()
+    items = root.findall(path)
+    return items
+
+
+def get_texts_from_items(items, field):
+    texts = []
+    for item in items:
+        texts.append(item.find(field).text)
+    return texts
+
+
+def get_words_from_texts(texts):
+    words = []
+    for text in texts:
+        text = text.split()
+        for word in text:
+            if len(word) > 6:
+                words.append(word)
+    return words
+
+
+def main():
+    tree = get_tree_from_xml('newsafr.xml')
+    items = get_items_from_tree(tree, 'channel/item')
+    texts = get_texts_from_items(items, 'description')
+    words = get_words_from_texts(texts)
+    pprint(Counter(words).most_common(10))
+
+
+main()
+
+
+
