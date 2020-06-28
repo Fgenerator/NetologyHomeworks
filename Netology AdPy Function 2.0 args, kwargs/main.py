ef94c78153fd08edbd5081691742312c6f101d4d
Evgeny Ermakov <desteev@gmail.com>
2020-06-28 17:28:45 +0500

    Minor edits

--- Netology AdPy Function 2.0 args, kwargs/main.py
+++ Netology AdPy Function 2.0 args, kwargs/main.py
@@ -1,15 +1,14 @@
-
-
 class Contact:
     def __init__(self,
                  first_name: str, second_name: str, phone: str,
+                 *args,
                  favorite=False,
-                 *args, **kwargs):
+                 **kwargs):
         self.first_name = first_name
         self.second_name = second_name
         self.phone = phone
         self.favorite = favorite
-        self.addition_args = [arg for arg in args]
+        self.addition_args = args
         self.addition_kwargs = kwargs
 
     def __str__(self):
@@ -17,7 +16,7 @@ class Contact:
         second_name_str = f'Second name: {self.second_name}\n'
         phone_str = f'Phone name: {self.phone}\n'
         is_favorite_str = f'Favorite: {self.is_favorite()}\n'
-        additional_str = f'{self.type_additional()}'
+        additional_str = f'{self.type_additional()}\n'
         return first_name_str + second_name_str + phone_str + is_favorite_str + additional_str
 
     def is_favorite(self):
@@ -34,76 +33,91 @@ class Contact:
             result = f'{result}\n\t{key} : {value}'
         return result
 
-# ```
-# Имя: Jhon
-# Фамилия: Smith
-# Телефон: +71234567809
-# В избранных: нет
-# Дополнительная информация:
-# 	 telegram : @jhony
-# 	 email : jhony@smith.com
-# ```
-
 
 class PhoneBook:
     def __init__(self, name: str):
         self.name = name
         self.contacts = []
 
-    def add_contact(self, contact: Contact):
-        self.contacts.append(contact)
+    def add_contact(self, new_contact: Contact):
+        result = [contact for contact in self.contacts
+                  if contact.phone == new_contact.phone]
+        if len(result):
+            for contact in result:
+                print(f'Contact {contact.first_name} {contact.second_name} '
+                      f'with phone {contact.phone} already in phonebook {self.name}.\n')
+        else:
+            self.contacts.append(new_contact)
+            print(f'{new_contact.first_name} {new_contact.second_name} added in phonebook {self.name}\n')
 
     def print_contacts(self):
+        print(f'Contacts in phonebook {self.name}:\n')
         for contact in self.contacts:
             print(contact)
 
     def delete_by_phone(self, phone):
-        for contact in self.contacts:
-            if contact.phone == phone:
+        result = [contact for contact in self.contacts
+                  if contact.phone == phone]
+        if len(result):
+            for contact in result:
                 self.contacts.remove(contact)
+                print(f'Contact {contact.first_name} {contact.second_name} with phone {phone} deleted.\n')
+        else:
+            print(f'{phone} not in phonebook {self.name}\n')
 
     def print_favorites(self):
-        for contact in self.contacts:
-            if contact.favorite:
+        result = [contact for contact in self.contacts
+                  if contact.favorite]
+        if len(result):
+            print(f'Favorites contacts in phonebook {self.name}:\n')
+            for contact in result:
                 print(contact)
+        else:
+            print(f'No favorites in phonebook {self.name}\n')
 
     def search_by_name(self, first_name, second_name):
-        sign = 1
-        for contact in self.contacts:
-            if contact.first_name == first_name and contact.second_name == second_name:
+        print(f'Search by name {first_name} {second_name}:')
+        result = [contact for contact in self.contacts
+                  if (contact.first_name == first_name) and (contact.second_name == second_name)]
+        if len(result):
+            for contact in result:
                 print(contact)
-                sign = 0
-        if sign:
-            print(f'{first_name} {second_name} not in phonebook {self.name}')
+        else:
+            print(f'{first_name} {second_name} not in phonebook {self.name}\n')
 
 
-# - Название телефонной книги - обязательное поле;
-# - Телефонная книга должна работать с классами Contact.
-#
-# Методы:
-# - Вывод контактов из телефонной книги;
-# - Добавление нового контакта;
-# - Удаление контакта по номеру телефона;
-# - Поиск всех избранных номеров;
-# - Поиск контакта по имени и фамилии.
+def adv_print(*args, start='', **kwargs):  # max_line
+    result = ''
+    start += result
+    return result
+
+# *3. Продвинутый print (необязательное задание)
+# Разработать свою реализацию функции print - adv_print. Она ничем не должна отличаться от классической функции кроме трех новых необязательных аргументов:
+# - start - с чего начинается вывод. По умолчанию пустая строка;
+# - max_line - максимальная длин строки при выводе. Если строка превышает max_line, то вывод автоматически переносится на новую строку;
+# - in_file - аргумент, определяющий будет ли записан вывод ещё и в файл.
 
 
 def main():
-    john = Contact('John', 'Smith', '+71234567809', False, 'Additional 1', 'Additional 2', telegram='@jhony',
+    john = Contact('John', 'Smith', '+71234567809', 'Additional 1', 'Additional 2', telegram='@jhony',
                    email='jhony@smith.com')
 
-    bob = Contact('Bob', 'Dilan', '+71234567808', True, 'Additional 1', 'Additional 2', telegram='@BobD',
+    bob = Contact('Bob', 'Dilan', '+71234567808', 'Additional 1', 'Additional 2', favorite=True, telegram='@BobD',
                   email='Bob@dilan.com')
+
+    bob2 = Contact('Bob', 'Dilan', '+71234567808', 'Additional 3', 'Additional 4', favorite=True, telegram='@BobD2',
+                  email='Bob2@dilan.com')
 
     book = PhoneBook('Phones')
     book.add_contact(john)
     book.add_contact(bob)
+    book.add_contact(bob2)
 
     book.print_favorites()
 
     book.print_contacts()
 
-    book.delete_by_phone('+71234567808')
+    book.delete_by_phone('+71234567809')
     book.print_contacts()
 
     book.search_by_name('Bob', 'Dilan')
