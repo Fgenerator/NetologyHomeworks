77b853c1cd30310f29b680c2b362ce4ae08e1cb2
Evgeny Ermakov <desteev@gmail.com>
2020-07-27 20:20:24 +0500

    Added balance_string

--- Netology AdPy Interview Stack/main.py
+++ Netology AdPy Interview Stack/main.py
@@ -9,34 +9,65 @@ class Stack:
         self.data.append(item)
 
     def pop(self) -> any:
-        return self.data.pop()
+        if not self.is_empty():
+            return self.data.pop()
 
     def peek(self) -> any:
-        return self.data[len(self.data)-1]
+        if not self.is_empty():
+            return self.data[len(self.data)-1]
 
     def size(self) -> int:
         return len(self.data)
 
 
-def balance_string(string):
-    ...
+def balance_string(string: str) -> str:
+    conformity = {
+        '}': '{',
+        ']': '[',
+        ')': '('
+    }
 
+    brackets = ['{', '}', '[', ']', '(', ')']
+
+    stack = Stack()
+
+    for symbol in string:
+        if symbol in brackets:
+            if symbol in conformity.values():
+                stack.push(symbol)
+            else:
+                if conformity[symbol] != stack.peek():
+                    return 'String is unbalanced'
+                else:
+                    stack.pop()
+    if stack.is_empty():
+        return 'String is balanced'
+    else:
+        return 'String is unbalanced'
+
+
+# завести пустой стек
+#
+# для каждого символа строки
+#   если он присутствует в наборе скобок
+#     если он отсутствует в словаре соответствий, т. е. это открывающая скобка
+#       добавить в стек этот символ
+#     иначе он закрывающая скобка
+#       если значение из словаря соответствий отличается от вершины стека
+#         return false
+#       иначе
+#         выкинуть последний элемент стека
+#
+# return стек пуст
 
 if __name__ == '__main__':
-    stack = Stack()
-    print(stack.is_empty())
-    print(stack.size())
+    print(balance_string('(((([{}]))))'))
+    print(balance_string('[([])((([[[]]])))]{()}'))
+    print(balance_string('{{[()]}}'))
+    print()
+    print(balance_string('}{}'))
+    print(balance_string('{{[(])]}}'))
+    print(balance_string('[[{())}]'))
 
-    stack.push(1)
-    stack.push(2)
-    stack.push(5)
 
-    print(stack.is_empty())
-    print(stack.size())
-
-    print(stack.peek())
-
-    print(stack.pop())
-
-    print(stack.size())
 
