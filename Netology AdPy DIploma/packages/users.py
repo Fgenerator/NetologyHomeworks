ad8b48b7c9df620d58b51893abd0f09df3937ad6
Evgeny Ermakov <desteev@gmail.com>
2020-08-03 16:17:30 +0500

    Added get_country_id, get_city_id, search_users

--- Netology AdPy DIploma/packages/users.py
+++ Netology AdPy DIploma/packages/users.py
@@ -37,5 +37,58 @@ class User:
         except AttributeError as e:
             print('Bad params attribute')
 
-    def search_users(self, age_from, age_to, sex, hometown, marriage):
-            ...
+    def get_country_id(self):
+        params = self.get_params()
+
+        try:
+            response = requests.get(
+                'https://api.vk.com/method/database.getCountries',
+                params
+            )
+        except Exception as e:
+            print('Something wrong with get country request')
+        try:
+            return response.json()['response']['items'][0]['id']
+        except KeyError as e:
+            print('Bad get country response')
+
+    def get_city_id(self, city: str):
+        params = self.get_params()
+        if len(city) > 15:
+            city = city[0:15]
+        params['q'] = city
+        params['country_id'] = self.get_country_id()
+        params['count'] = 1
+
+        try:
+            response = requests.get(
+                'https://api.vk.com/method/database.getCities',
+                params
+            )
+        except Exception as e:
+            print('Something wrong with get cities request')
+        try:
+            return response.json()['response']['items'][0]['id']
+        except KeyError as e:
+            print('Bad get cities response')
+
+    def search_users(self, age_from: int, age_to: int, sex: int, city: str, status: int):
+        params = self.get_params()
+        params['age_from'] = age_from
+        params['age_to'] = age_to
+        params['sex'] = sex
+        params['city'] = self.get_city_id(city)
+        params['status'] = status
+        params['count'] = 100
+
+        try:
+            response = requests.get(
+                'https://api.vk.com/method/users.search',
+                params
+            )
+        except Exception as e:
+            print('Something wrong with users search request')
+        try:
+            return response.json()['response']['items']
+        except KeyError as e:
+            print('Bad get users search response')
