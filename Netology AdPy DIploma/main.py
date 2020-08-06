498be36ba26cd7e9913c147c28e933ab543c8a7e
Evgeny Ermakov <desteev@gmail.com>
2020-08-06 16:52:27 +0500

    Edited prepare_users() Added start(), main()

--- Netology AdPy DIploma/main.py
+++ Netology AdPy DIploma/main.py
@@ -1,20 +1,19 @@
 from vkauth import VKAuth
 from users import User
-import db
+import mongo
 from pprint import pprint
 from operator import itemgetter
 import json
 import time
 
 
-def prepare_users(users_from_vk, token):
+def prepare_users(users_from_vk, token, db):
     internal_users = []
     for vk_user in users_from_vk:
         internal_user = User(str(vk_user['id']), token)
         internal_user.closed = vk_user['is_closed']
-        if not internal_user.closed:
+        if not internal_user.closed and not mongo.is_user_in_db(internal_user, db):
             internal_users.append(internal_user)
-    # print('internal users prepared')
     return internal_users
 
 
@@ -26,7 +25,6 @@ def prepare_photos(photos):
             'likes': photo['likes']['count'],
             'comments': photo['comments']['count']
         })
-    # print('internal photos prepared')
     return internal_photos
 
 
@@ -35,7 +33,6 @@ def sort_photos(internal_photos):
     # internal_photos = sorted(internal_photos, key=itemgetter('likes', 'comments'))
     internal_photos.sort(key=itemgetter('likes', 'comments'))
     internal_photos.reverse()
-    # print('internal photos sorted')
     return internal_photos
 
 
@@ -52,61 +49,91 @@ def prepare_data_to_json(internal_users):
             'page': int_user.page,
             'photos': get_top_3_photos(sort_photos(prepare_photos(int_user.get_photos())))
         })
-    print('data prepared to json')
+    #print('data prepared to json')
     return data
 
 
-def write_data_to_json(data):
+def write_data_to_json(data_to_write):
     with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+        json.dump(data_to_write, f, ensure_ascii=False, indent=4)
+
+    print('Result writed to data.json file')
 
 
-vk_auth = VKAuth(['photos', 'friends'], '7556238', '5.89')
+def start(user, token, db):
+    print('VKinder')
 
-vk_auth.auth()
+    while True:
+        print('Список команд: ')
+        print('1 - начать новый поиск;')
+        print('0 - выход.')
 
-access_token = vk_auth.get_token()
-user_id = vk_auth.get_user_id()
+        user_input = input('> ')
 
-user = User(user_id, access_token)
+        if user_input == '1':
+            age_from = input('Введите нижнюю границу возраста для поиска: ')
+            age_to = input('Введите верхнюю границу возраста для поиска: ')
+            sex = input('Введите пол (0 - любой, 1 - жен, 2 - муж): ')
+            city = input('Введите город поиска: ')
+            print('Введите семейное положение.')
+            print('1 — не женат (не замужем);\n2 — встречается;\n3 — помолвлен(-а);\n4 — женат (замужем);\n'
+                  '5 — всё сложно;\n6 — в активном поиске;\n7 — влюблен(-а);\n8 — в гражданском браке.')
+            status = input('> ')
 
-users = prepare_users(user.search_users(20, 25, 1, 'Тюмень', 6), access_token)
+            users = prepare_users(user.search_users(age_from, age_to, sex, city, status), token, db)
 
-data = prepare_data_to_json(users)
+            data = prepare_data_to_json(users)
 
-write_data_to_json(data)
+            write_data_to_json(data)
 
-vk_users_db = db.prepare_db()
+            mongo.read_data(data, db)
+        elif user_input == '0':
+            break
+        else:
+            print('Такой команды нет')
 
-db.read_data(data, vk_users_db)
 
-pprint(list(vk_users_db.users.find()))
+def main():
+    vk_auth = VKAuth(['photos', 'friends'], '7556238', '5.89')
 
-pprint(len(list(vk_users_db.users.find())))
+    vk_auth.auth()
 
-# vk_users_db.users.delete_many({})
+    print('Authorized successfully\n')
 
-"""
+    access_token = vk_auth.get_token()
+    user_id = vk_auth.get_user_id()
+    vk_users_db = mongo.prepare_db()
 
-Поиск людей
-    Ввод информации
-        Если информация в профиле не указана, то спросить для поиска
+    user = User(user_id, access_token)
 
-    Искать людей, подходящих под условия, на основании информации о пользователе из VK:
-        - возраст,
-        - пол,
-        - город,
-        - семейное положение
+    start(user, access_token, vk_users_db)
 
-Вывод в JSON
-    JSON-файл с 10 объектами, где у каждого объекта перечислены топ-3 фотографии
-    и ссылка на аккаунт.
 
-БД
-    Таблица с пользователями сервиса
-    Таблица с пользователями вк
+if __name__ == '__main__':
+    main()
 
-Тесты
-    pytest
+# vk_auth = VKAuth(['photos', 'friends'], '7556238', '5.89')
+#
+# vk_auth.auth()
+#
+# access_token = vk_auth.get_token()
+# user_id = vk_auth.get_user_id()
+# vk_users_db = mongo.prepare_db()
 
-"""
+
+#
+# user = User(user_id, access_token)
+#
+# users = prepare_users(user.search_users(20, 25, 1, 'Тюмень', 6), access_token, vk_users_db)
+#
+# data = prepare_data_to_json(users)
+#
+# write_data_to_json(data)
+#
+# mongo.read_data(data, vk_users_db)
+#
+# pprint(list(vk_users_db.users.find()))
+#
+# pprint(len(list(vk_users_db.users.find())))
+#
+# #vk_users_db.users.delete_many({})
