2aa490123c835e74bb5df3722a44dec4d370e0aa
Evgeny Ermakov <desteev@gmail.com>
2020-08-06 16:53:09 +0500

    Added get_pages_from_collection(), is_user_in_db()

---
+++ Netology AdPy DIploma/packages/mongo.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+
+
+def prepare_db():
+    client = MongoClient()
+    db = client['vk_users']
+    return db
+
+
+def read_data(data, db):
+    vk_users_collection = db.users
+    vk_users_collection.insert_many(data)
+
+
+def get_pages_from_collection(db):
+    pages = []
+    for user in list(db.users.find()):
+        pages.append(user['page'])
+    return pages
+
+
+def is_user_in_db(internal_user, db):
+    return internal_user.page in get_pages_from_collection(db)
+
+
+
