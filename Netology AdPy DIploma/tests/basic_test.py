23542ddc4aa1b62105b092e5b67f2157e24f37c3
Evgeny Ermakov <desteev@gmail.com>
2020-08-13 20:28:43 +0500

    Added: prepare_temp_db(), prepare_users(), test_prepare_data()

--- Netology AdPy DIploma/tests/basic_test.py
+++ Netology AdPy DIploma/tests/basic_test.py
@@ -1,12 +1,11 @@
-from packages.vkauth import VKAuth
 from packages.users import User
 import packages.mongo as mongo
 
 from unittest.mock import patch
 
-import pytest
-
 import main
+
+from pymongo import MongoClient
 
 
 def auth():
@@ -16,16 +15,24 @@ def auth():
     return access_token, user_id
 
 
-def create_user():
-    access_token, user_id = auth()
+def create_user(access_token=None, user_id=None):
+    if not access_token and not user_id:
+        access_token, user_id = auth()
     user = User(user_id, access_token)
     return user
 
 
-def search_users():
-    user = create_user()
+def search_users(user=None):
+    if not user:
+        user = create_user()
     users = user.search_users(20, 25, 1, 'Москва', 6)
     return users
+
+
+def prepare_temp_db():
+    client = MongoClient()
+    db = client['temp_db']
+    return db
 
 
 def test_auth():
@@ -63,8 +70,34 @@ def test_negative_search_users():
 
 
 def test_prepare_users():
+    db = prepare_temp_db()
+    token = auth()[0]
     users = search_users()
-    users = main.prepare_users(users)
+    users = main.prepare_users(users, token, db)
 
     for user in users:
         assert user.id
+
+
+def prepare_users():
+    db = prepare_temp_db()
+    token = auth()[0]
+    users = search_users()
+    users = main.prepare_users(users, token, db)
+
+    return users
+
+
+def test_prepare_data():
+    users = prepare_users()
+    users = main.prepare_data_to_json(users)
+    for user in users:
+        assert user['photos']
+
+
+def test_write_data_to_json():
+    ...
+
+
+def test_write_data_to_db():
+    ...
